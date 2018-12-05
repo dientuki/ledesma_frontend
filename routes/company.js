240f2b6fb52a00b45b9d3b453ca784b60578f744
Juan Farias <dientuki@gmail.com>
2018-12-05 01:21:23 -0300

    cname

--- routes/company.js
+++ routes/company.js
@@ -4,6 +4,7 @@ module.exports = {
     let bestCompany = "SELECT color, team, sum(point) as teamPoint FROM `points` INNER JOIN company2team ON points.fk_c2t = company2team.id INNER JOIN teams ON company2team.fk_team = teams.id where fk_company = " + company + " group by fk_team order by teamPoint desc limit 1"; // query database to get all the players
     let bestTeam = "SELECT color, company, sum(point) as teamPoint FROM `points`   INNER JOIN company2team ON points.fk_c2t = company2team.id   INNER JOIN teams ON company2team.fk_team = teams.id   INNER JOIN business ON company2team.fk_company = business.id   group by fk_team, fk_company order by teamPoint desc limit 1";
     let bestGlobal = "SELECT company, SUM(point) as totalPoints FROM `points`  INNER JOIN company2team ON points.fk_c2t = company2team.id  INNER JOIN business ON company2team.fk_company = business.id group by fk_company order by totalPoints desc limit 1"; // query database to get all the players
+    let companyName = "SELECT company FROM business WHERE id =" + company;
 
     // execute query
     db.query(bestCompany, (err, resultBestCompany) => {
@@ -21,14 +22,20 @@ module.exports = {
             res.redirect('/');
           }
 
-          res.render('companyscore.ejs', {
-            title: "Ganadores del juego",
-            company: resultBestCompany,
-            team: resultBestTeam,
-            business: resultBestGlobal,
-            css: css
-          });
+          db.query(companyName, (err, cname) => {
+            if (err) {
+              res.redirect('/');
+            }
 
+            res.render('companyscore.ejs', {
+              title: "Ganadores del juego",
+              company: resultBestCompany,
+              team: resultBestTeam,
+              business: resultBestGlobal,
+              cname: cname,
+              css: css
+            });
+          });
         });
 
       });
--- routes/perteam.js
+++ routes/perteam.js
@@ -2,9 +2,9 @@ module.exports = {
   getPerTeamPage: (req, res) => {
     let numberPattern = /\d+/g;
     let company = req.params.company.match( numberPattern )
-    //console.log(req.params.company.match( numberPattern ))
     let queryTeam = "SELECT team, color, sum(point) as teamPoints FROM `points` INNER JOIN company2team ON points.fk_c2t = company2team.id INNER JOIN teams ON company2team.fk_team = teams.id where fk_company = " + company + "  group by fk_team order by teamPoints desc"; // query database to get all the players
     let queryGlobal = "SELECT color, company, sum(point) as teamPoint FROM `points`   INNER JOIN company2team ON points.fk_c2t = company2team.id   INNER JOIN teams ON company2team.fk_team = teams.id   INNER JOIN business ON company2team.fk_company = business.id   group by fk_team, fk_company order by teamPoint desc limit 10";
+    let companyName = "SELECT company FROM business WHERE id =" + company;
 
     // execute query
     db.query(queryTeam, (err, resultCompany) => {
@@ -16,12 +16,22 @@ module.exports = {
         if (err) {
           res.redirect('/');
         }
-        res.render('company.ejs', {
-          title: "Posiciones por equipos",
-          company: resultCompany,
-          globalres: resultGlobal,
-          css: css
+
+        db.query(companyName, (err, cname) => {
+          if (err) {
+            res.redirect('/');
+          }
+
+          res.render('company.ejs', {
+            title: "Posiciones por equipos",
+            company: resultCompany,
+            globalres: resultGlobal,
+            cname: cname,
+            css: css
+          });
         });
+
+
       });
     });
   }
